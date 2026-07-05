6f0698370269c582d7bdcbfa81928e516cdf65cd
HalfCooler <79041742+HalfCooler@users.noreply.github.com>
2026-07-06 04:09:29 +0800

    修正泵在多次超频升级下工作不正常

--- src/main/java/ic2/core/util/PumpUtil.java
+++ src/main/java/ic2/core/util/PumpUtil.java
@@ -1,10 +1,13 @@
 package ic2.core.util;
 
+import java.util.ArrayDeque;
 import java.util.HashSet;
+import java.util.Queue;
 import java.util.Set;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.BlockPos.MutableBlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LiquidBlock;
@@ -13,6 +16,11 @@ import net.minecraftforge.fluids.IFluidBlock;
 
 public class PumpUtil
 {
+	private static final int MAX_TRACE_STEPS = 64;
+	private static final int MAX_AIR_BRIDGE = 64;
+	private static final int LOCAL_AIR_BRIDGE = 8;
+	private static final int FALLBACK_RADIUS = 2;
+
 	private static int moveUp(Level world, MutableBlockPos pos)
 	{
 		pos.set(pos.getX(), pos.getY() + 1, pos.getZ());
@@ -90,11 +98,28 @@ public class PumpUtil
 
 	public static BlockPos searchFluidSource(Level world, BlockPos startPos)
 	{
+		return searchFluidSource(world, startPos, false);
+	}
+
+	public static BlockPos searchFluidSource(Level world, BlockPos startPos, boolean simulate)
+	{
 		MutableBlockPos pos = new MutableBlockPos();
 		pos.set(startPos.getX(), startPos.getY(), startPos.getZ());
+
+		if (getFlowDecay(world, pos) < 0)
+		{
+			BlockPos fluidEntry = findNearestHorizontalFluid(world, startPos, MAX_AIR_BRIDGE);
+			if (fluidEntry == null)
+			{
+				return scanAreaForSource(world, startPos, simulate);
+			}
+
+			pos.set(fluidEntry.getX(), fluidEntry.getY(), fluidEntry.getZ());
+		}
+
 		int decay = getFlowDecay(world, pos);
 
-		for (int i = 0; i < 64; i++)
+		for (int i = 0; i < MAX_TRACE_STEPS; i++)
 		{
 			int newDecay = moveUp(world, pos);
 			if (newDecay < 0)
@@ -102,6 +127,14 @@ public class PumpUtil
 				newDecay = moveSideways(world, pos, decay);
 				if (newDecay < 0)
 				{
+					BlockPos bridge = findNearestHorizontalFluid(world, pos, LOCAL_AIR_BRIDGE);
+					if (bridge != null)
+					{
+						pos.set(bridge.getX(), bridge.getY(), bridge.getZ());
+						decay = getFlowDecay(world, pos);
+						continue;
+					}
+
 					break;
 				}
 			}
@@ -112,7 +145,7 @@ public class PumpUtil
 		Set<BlockPos> visited = new HashSet<>(64);
 		int i = 0;
 
-		while (i < 64)
+		while (i < MAX_TRACE_STEPS)
 		{
 			label85:
 			{
@@ -127,7 +160,7 @@ public class PumpUtil
 						{
 							if (newDecay == 0)
 							{
-								return pos;
+								return pos.immutable();
 							}
 							break label103;
 						}
@@ -141,7 +174,7 @@ public class PumpUtil
 						{
 							if (newDecay == 0)
 							{
-								return pos;
+								return pos.immutable();
 							}
 							break label103;
 						}
@@ -155,7 +188,7 @@ public class PumpUtil
 						{
 							if (newDecay == 0)
 							{
-								return pos;
+								return pos.immutable();
 							}
 							break label103;
 						}
@@ -175,10 +208,18 @@ public class PumpUtil
 
 					if (newDecay == 0)
 					{
-						return pos;
+						return pos.immutable();
 					}
 				}
 
+				i++;
+				continue;
+			}
+
+			BlockPos bridge = findNearestHorizontalFluid(world, pos, LOCAL_AIR_BRIDGE);
+			if (bridge != null)
+			{
+				pos.set(bridge.getX(), bridge.getY(), bridge.getZ());
 				i++;
 				continue;
 			}
@@ -187,34 +228,121 @@ public class PumpUtil
 			break;
 		}
 
-		MutableBlockPos cPos = new MutableBlockPos();
-
-		for (int ix = -2; ix <= 2; ix++)
+		BlockPos result = scanAreaForSource(world, pos, simulate);
+		if (result != null)
 		{
-			for (int iz = -2; iz <= 2; iz++)
-			{
-				cPos.set(pos.getX() + ix, pos.getY(), pos.getZ() + iz);
-				BlockState state = world.getBlockState(cPos);
-				decay = getFlowDecay(state, world, cPos);
-				if (decay >= 0)
-				{
-					if (decay == 0)
-					{
-						return cPos;
-					}
+			return result;
+		}
 
-					if (decay >= 1 && decay < 7 && state.getBlock() instanceof LiquidBlock)
-					{
-						world.setBlock(cPos, state.setValue(LiquidBlock.LEVEL, decay + 1), 3);
-					} else
-					{
-						world.removeBlock(cPos, false);
-					}
+		if (!pos.equals(startPos))
+		{
+			return scanAreaForSource(world, startPos, simulate);
+		}
+
+		return null;
+	}
+
+	private static BlockPos findNearestHorizontalFluid(Level world, BlockPos origin, int maxDist)
+	{
+		if (getFlowDecay(world, origin) >= 0)
+		{
+			return origin;
+		}
+
+		Queue<BlockPos> queue = new ArrayDeque<>();
+		Set<BlockPos> visited = new HashSet<>();
+		queue.add(origin);
+		visited.add(origin);
+		int y = origin.getY();
+
+		while (!queue.isEmpty())
+		{
+			BlockPos current = queue.poll();
+			int dx = Math.abs(current.getX() - origin.getX());
+			int dz = Math.abs(current.getZ() - origin.getZ());
+			if (dx + dz > maxDist)
+			{
+				continue;
+			}
+
+			for (Direction dir : Util.HORIZONTAL_DIRS)
+			{
+				BlockPos next = current.relative(dir);
+				if (next.getY() != y || !visited.add(next))
+				{
+					continue;
+				}
+
+				if (getFlowDecay(world, next) >= 0)
+				{
+					return next;
+				}
+
+				if (world.getBlockState(next).isAir())
+				{
+					queue.add(next);
 				}
 			}
 		}
 
 		return null;
+	}
+
+	private static BlockPos scanAreaForSource(Level world, BlockPos center, boolean simulate)
+	{
+		MutableBlockPos cPos = new MutableBlockPos();
+
+		for (int ix = -FALLBACK_RADIUS; ix <= FALLBACK_RADIUS; ix++)
+		{
+			for (int iz = -FALLBACK_RADIUS; iz <= FALLBACK_RADIUS; iz++)
+			{
+				cPos.set(center.getX() + ix, center.getY(), center.getZ() + iz);
+				BlockState state = world.getBlockState(cPos);
+				int decay = getFlowDecay(state, world, cPos);
+				if (decay < 0)
+				{
+					continue;
+				}
+
+				if (decay == 0)
+				{
+					return cPos.immutable();
+				}
+
+				if (!simulate)
+				{
+					applyFallbackFluidStep(world, cPos, state);
+				}
+			}
+		}
+
+		return null;
+	}
+
+	private static void applyFallbackFluidStep(Level world, BlockPos pos, BlockState state)
+	{
+		Block block = state.getBlock();
+		if (block instanceof LiquidBlock)
+		{
+			int level = state.getValue(LiquidBlock.LEVEL);
+			if (level == 0)
+			{
+				return;
+			}
+
+			if (level < 15)
+			{
+				world.setBlock(pos, state.setValue(LiquidBlock.LEVEL, level + 1), 3);
+			}
+			else
+			{
+				world.removeBlock(pos, false);
+			}
+		}
+		else
+		{
+			world.removeBlock(pos, false);
+		}
 	}
 
 	protected static int getFlowDecay(Level world, BlockPos pos)
